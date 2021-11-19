7a5084e0c483654a2aeff5d42be53df79b8fb11d
Shwelinn <33347791+Shwelinn@users.noreply.github.com>
2021-11-19 02:44:48 +0100

    Update gini-index.py

--- gini-index.py
+++ gini-index.py
@@ -6,7 +6,7 @@ from pandas_profiling import ProfileReport
 st.title("Sample project: GINI Index across the countries from 1978-2007.")
 st.markdown("**In economics, the Gini coefficient, also the Gini index and the Gini ratio, is a measure of statistical dispersion intended to represent the income inequality or the wealth inequality within a nation or a social group. The Gini coefficient was developed by the statistician and sociologist Corrado Gini. The Gini coefficient measures the inequality among values of a frequency distribution (for example, levels of income). A Gini coefficient of zero expresses perfect equality, where all values are the same (for example, where everyone has the same income). A Gini coefficient of one (or 100%) expresses maximal inequality among values (e.g., for a large number of people where only one person has all the income or consumption and all others have none, the Gini coefficient will be nearly one).**")
 
-df = pd.read_csv("data\gini.csv")
+df = pd.read_csv("gini.csv")
 df.dropna(axis = 0, inplace = True)
 
 options_one = list(df['Country'])
